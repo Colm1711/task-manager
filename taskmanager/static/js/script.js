ce7662448838bb1ef13a8e3b155b7d9b090e38d5
Colm1711 <cosullivan1711@gmail.com>
2022-08-03 18:47:47 +0000

    Add update tasks page and form

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    var selects = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(selects);
 });
