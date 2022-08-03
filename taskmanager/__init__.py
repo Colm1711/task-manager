c7bd426ed8cb1aac490768246dd2092486bd4734
Colm1711 <cosullivan1711@gmail.com>
2022-08-03 19:51:41 +0000

    updating init file

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -9,7 +9,11 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 # create 2 app configuration vars
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 # create instance of the SQLAlchemy class and pass it the instance of the flask app
 db = SQLAlchemy(app)
